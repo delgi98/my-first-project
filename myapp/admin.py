99fdcb0f527da794b34413266935c482ec47c562
delgi98 <mcshinoda761@gmail.com>
2023-06-21 09:18:18 +0700

    updated admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Pilihan, Pertanyaan
 
 # Register your models here.
+class PilihanAdmin(admin.StackedInline):
+    model = Pilihan
+    extra = 4
+    
+class PertanyaanAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["pertanyaan_text"]}),
+        ("informasi tanggal", {"fields": ["tanggal_post"], "classes": ["collapse"]}),
+    ]
+    inlines = [PilihanAdmin]
+    
+admin.site.register(Pertanyaan, PertanyaanAdmin)
